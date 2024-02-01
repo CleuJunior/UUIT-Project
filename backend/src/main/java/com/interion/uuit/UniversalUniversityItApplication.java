4acdb01eb0e0c4b976a071afa0ccfd57c24810e2
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-02-01 19:56:27 +0600

    feat(backend): Initial Backend Project

---
+++ backend/src/main/java/com/interion/uuit/UniversalUniversityItApplication.java
@@ -0,0 +1,13 @@
+package com.interion.uuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalUniversityItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalUniversityItApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/interion/uuit/UniversalUniversityItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interion.uuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalUniversityItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
